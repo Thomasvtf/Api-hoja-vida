a499d7887b96c158a0c0ccdc1a13eceff8f0b06f
Thomasvtf <thomas.vargas899@gmail.com>
2026-09-01 13:24:10 -0500

    Correo con errores

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from database import conectar_bd
 app = Flask(__name__)
 
@@ -15,6 +15,52 @@ def probar_bd():
         return {
             "mensaje":"database conectada"
         }
+        
+@app.route("/api/registro-hoja-vida", methods = ["POST"])
+def registro_hoja_vida():
+    conec = conectar_bd()
+    cursor = conec.cursor()
+    datos = request.json
+    
+    correo_nuevo = datos ["correo"]
+    
+    #Consultar si el correo ya existe
+    cursor.execute("SELECT correo FROM hojas_vida WHERE correo = %s", (correo_nuevo,))
+    busqueda = cursor.fetchone()
+    cursor.close()
+    conec.close()
+    
+    if busqueda is not None:
+        correo_sql = busqueda[0]
+        
+        if correo_sql == correo_nuevo:
+            print("El correo ya existe")
+            return True
+        print ("El correo está disponible")
+        return False
+        
+    sql = """INSERT INTO hojas_vida (nombre, edad, ciudad, correo, fotografia, programa, ficha, jornada) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    valor = (
+        datos ["nombre"],
+        datos ["edad"],
+        datos ["ciudad"],
+        datos ["correo"],
+        datos.get("fotografia"),
+        datos ["programa"],
+        datos ["ficha"],
+        datos ["programa"]
+    )
+    
+    cursor.execute(sql,valor)
+    conec.commit()
+    
+    #Manejo del id de la hoja de vida
+    id_generado = cursor.lastrowid
+    
+    cursor.close()
+    conec.close()
+    
+    return {"Mensaje":"Hoja de vida creada","id": id_generado}
 
 @app.route("/api/hojas-vida/<int:id>")
 def obtener_hojasvidaid(id):
@@ -33,6 +79,7 @@ def obtener_hojasvida():
             "nombre":"Low",
             "edad": 36,
             "ciudad":"nuevo mundo",
+            "correo":"low@email.com",
             "fotografia":"foto",
             "programa":"ADSO",
             "ficha":2222,
@@ -43,6 +90,7 @@ def obtener_hojasvida():
             "nombre":"Usop",
             "edad": 27,
             "ciudad":"nuevomundo",
+            "correo":"usop@email.com",
             "fotografia":"foto",
             "programa":"fotografia",
             "ficha":4444,
