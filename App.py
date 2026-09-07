380da07ab077a6809bb032e64f844534c5be7836
Thomasvtf <thomas.vargas899@gmail.com>
2026-09-07 13:00:02 -0500

    Encontrar por id, actualizar y eliminar

--- App.py
+++ App.py
@@ -58,11 +58,26 @@ def registro_hoja_vida():
     
     return {"Mensaje":"Hoja de vida creada","id": id_generado}
 
-@app.route("/api/hojas-vida/<int:id>")
+@app.route("/api/hojas-vida/<int:id>", methods=["GET"])
 def obtener_hojasvidaid(id):
-    return{
-        "Mensaje":"Hoja de vida encontrada","id":id
-    }
+    
+    conec = conectar_bd()
+    cursor = conec.cursor(dictionary = True)
+    
+    cursor.execute("SELECT * FROM hojas_vida WHERE id = %s", (id,))
+    
+    datos = cursor.fetchone()
+    
+    
+    cursor.close()
+    conec.close()
+    
+    #Qué pasa cuando es nulo
+    if datos is None:
+        return{"No se encontro la hoja de vida"}
+    else:
+        return datos
+        
 
 @app.route("/api/hojas-vida", methods = ["GET"])
 def obtener_hojasvida():
@@ -79,6 +94,79 @@ def obtener_hojasvida():
     conec.close()
     
     return {"Mensaje":"Listado de hojas de vida", "":listado }
+
+#Eliminar hoja de vida
+@app.route("/api/eliminar-hoja-vida/<int:id>", methods = ["DELETE"])
+def eliminar_hoja_vida(id):
+    conec = conectar_bd()
+    cursor = conec.cursor()
+    
+    cursor.execute("SELECT id FROM hojas_vida WHERE id = %s", (id,))
+    
+    datos = cursor.fetchone()
+    
+    if datos is None:
+        cursor.close()
+        conec.close()
+        return{"Mensaje":"El id no existe"}
+    else:
+        cursor.execute("DELETE FROM hojas_vida WHERE id = %s", (id,))
+     
+        conec.commit()
+        
+        cursor.close()
+        conec.close()
+        
+        return {"Mensaje":"Registro eliminado"}
+    
+#Actualizar hoja de vida
+@app.route("/api/actualizar-hoja-vida/<int:id>", methods = ["PUT"])
+def actualizar_hoja_vida(id):
+    datos = request.json
+    conec = conectar_bd()
+    cursor = conec.cursor(buffered = True)
+    
+    buscar = """SELECT id FROM hojas_vida WHERE id = %s"""
+    cursor.execute(buscar,(id,))
+
+    result = cursor.fetchone()
+    
+    if result is None:
+        cursor.close()
+        conec.close()
+        return {"Mensaje":"No se encontro la hoja de vida"}, 404
+    
+    #Validar correo
+    sql = """SELECT id FROM hojas_vida WHERE correo = %s and id != %s"""
+    cursor.execute(sql, (datos["correo"], id))
+    resul = cursor.fetchone()
+    
+    if resul is not None:
+        cursor.close()
+        conec.close()
+        return{"Mensaje":"El correo ya esta en uso."}, 400
+
+    #Actualizar hoja vida
+    sql1 = """UPDATE hojas_vida SET nombre = %s, edad = %s, ciudad = %s, correo = %s, fotografia = %s, programa = %s, ficha = %s, jornada = %s WHERE id = %s"""
+    valor = (
+            datos ["nombre"],
+            datos ["edad"],
+            datos ["ciudad"],
+            datos ["correo"],
+            datos.get("fotografia"),
+            datos ["programa"],
+            datos ["ficha"],
+            datos ["programa"],
+            id
+        )
+
+    cursor.execute(sql1,valor)
+    conec.commit()
+    
+    cursor.close()
+    conec.close()
+    
+    return {"Mensaje":"Hoja de vida actualizada","id": id}, 200
 
 if __name__ == '__main__':
     app.run(debug = True)
